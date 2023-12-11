6718fd94350169372b7d5705165aa608ac692aed
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2023-12-11 09:32:46 -0500

    Proceso asíncrono..cambia el texto..cambian los botones...

--- src/app/components/clases/clases.component.ts
+++ src/app/components/clases/clases.component.ts
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
 export class ClasesComponent {
 
   alerta:string="alert-danger"
+  loading:boolean=false
 
+  propiedades:Object={
+    danger:false
+  }
+
+  ejecutar(){
+    this.loading=true;
+    setTimeout(()=> this.loading=false, 3000)
+  }
 }

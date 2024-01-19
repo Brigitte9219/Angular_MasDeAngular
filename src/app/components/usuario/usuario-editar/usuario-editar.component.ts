48fb9a50b7505b9cc20a50f85dd0552dd01decc3
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-01-19 16:21:45 -0500

    Obteniendo parametros del padre, desde las rutas hijas - segunda parte

--- src/app/components/usuario/usuario-editar/usuario-editar.component.ts
+++ src/app/components/usuario/usuario-editar/usuario-editar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';//Obtener el parámetro (id)
 
 @Component({
   selector: 'app-usuario-editar',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./usuario-editar.component.css']
 })
 export class UsuarioEditarComponent {
+
+  constructor( private router:ActivatedRoute ){
+    this.router.parent?.params.subscribe( parametros => {
+      console.log('RUTA HIJA Usuario Editar');
+      console.log('parametros');
+    })
+  }
 
 }
